4159f3cfb1307b3b75e85d860be97cc813afb579
SuperHuangXu <www.1567891@qq.com>
2021-11-09 12:31:46 +0800

    feat: 🎸 update RBE-primitives

--- RBE-primitives/src/main.rs
+++ RBE-primitives/src/main.rs
@@ -19,6 +19,16 @@ fn main() {
     // 数组
     let v = [1; 3];
     println!("{:?}", v);
+    // 数组是在栈中分配的
     println!("array occupies {} bytes", mem::size_of_val(&v));
+
+    // 数组可以自动被借用为切片
+    analyze_slice(&v);
+    // 切片
     println!("{:?}", &v[0..2]);
+}
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
 }
