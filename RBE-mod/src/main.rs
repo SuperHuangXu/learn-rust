2469753aa66834577573354b994e8a92e3c64137
SuperHuangXu <www.1567891@qq.com>
2021-11-10 15:18:09 +0800

    feat: 🎸 add RBE-mod

---
+++ RBE-mod/src/main.rs
@@ -0,0 +1,24 @@
+use deeply::nested::function as other_function;
+
+fn main() {
+    other_function();
+    println!("Entering block");
+    {
+        use deeply::nested::function;
+        function();
+    }
+    function();
+}
+
+fn function() {
+    println!("called `function()`");
+}
+
+mod deeply {
+    pub mod nested {
+        pub fn function() {
+            println!("called `deeply::nested::function()`")
+        }
+    }
+}
+
